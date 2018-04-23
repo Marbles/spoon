b73247410781f825535b18b4a86ee2aa0bd0e601
Ferre <ferrelambert@hotmail.com>
2018-04-23 14:54:36 +0200

    Add and update packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,24 +1,125 @@
+// Set the mix variable
+/* global path */
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
+/**
+ * Packages used for build
  */
-const tailwindcss = require('tailwindcss');
+const webpack = require('webpack');
+const moment = require('moment');
+const gitRevSync = require('git-rev-sync');
+const htmlCritical = require('html-critical-webpack-plugin');
+const copy = require('copy');
 
+// package vars
+const pkg = require('./package.json');
+
+// Our banner
+const banner = (function () {
+    return [
+        '/**',
+        ' * @project        Marbles Website',
+        ' * @author         Marbles',
+        ' * @build          ' + moment().format('llll') + ' GMT+1',
+        ' * @release        ' + gitRevSync.long() + ' [' + gitRevSync.branch() + ']',
+        ' * @copyright      Copyright (c) ' + moment().format('YYYY') + ', Marbles',
+        ' *',
+        ' */',
+        '',
+    ].join('\n');
+})();
+
+/**
+ * Webpack plugins
+ */
+const webpackPlugins = [
+    new webpack.BannerPlugin({
+        banner: banner,
+        raw: true,
+    }),
+];
+
+/**
+ * Webpack plugins that only need to be run in production
+ */
+if (mix.inProduction()) {
+    /*
+    const criticalUrls = [
+        { url: '', template: 'index' },
+    ];
+
+    criticalUrls.forEach(function (element) {
+        const criticalSrc = process.env.BASE_URL + element.url;
+        const criticalDest = './templates/' + element.template + '_critical.min.css';
+
+        webpackPlugins = webpackPlugins.concat([
+            new htmlCritical({
+                src: criticalSrc,
+                dest: criticalDest,
+                penthouse: {
+                    blockJSRequests: false,
+                    forceInclude: [],
+                },
+                inline: false,
+                ignore: [],
+                css: [
+                    './web/dist/css/site.css',
+                ],
+                minify: true,
+                width: 1200,
+                height: 1200,
+            }),
+        ]);
+    });
+    */
+}
+
+/**
+ * Start the Mix function
+ */
 mix
     .options({
-        processCssUrls: false,
+        processCssUrls: false, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
         postCss: [
-            tailwindcss('./tailwind.js'),
-        ]
+            require('postcss-object-fit-images'),
+            require('tailwindcss')('./tailwind.js'),
+        ],
     })
-    .version()
-    .js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/scss/app.scss', 'public/css');
+    .webpackConfig({
+        plugins: webpackPlugins,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'eslint-loader',
+                    options: {
+                        cache: true,
+                    },
+                },
+            ],
+        },
+    })
+    .babelConfig({
+        'presets': ['env'],
+        'compact': true,
+    })
+    .setPublicPath('./public')
+    .js('./resources/assets/js/app.js', './public/js/')
+    .extract(Object.keys(pkg.dependencies))
+    .sass('./resources/assets/scss/app.scss', './public/css')
+    .autoload({
+        'jquery': ['$', 'window.jQuery', 'jQuery'],
+        'vue': ['Vue','window.Vue'],
+    })
+    .purgeCss({
+        globs: [
+            path.join(__dirname, '/resources/views/**/*.{php}'),
+            path.join(__dirname, '/resources/assets/**/*.{js,scss,vue}'),
+        ],
+        extensions: ['html', 'js', 'php', 'vue', 'twig', 'scss', 'css'],
+        whitelistPatterns: [],
+        whiteList: [],
+    })
+    .version();
